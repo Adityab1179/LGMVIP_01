dc2757130be7681f1ad8d2f46987c5a04dacb21e
Aditya Bansal <110539909+Adityab1179@users.noreply.github.com>
2023-08-30 10:00:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const taskInput = document.getElementById("taskInput");
+const addTaskBtn = document.getElementById("addTaskBtn");
+const taskList = document.getElementById("taskList");
+
+addTaskBtn.addEventListener("click", () => {
+  const taskText = taskInput.value.trim();
+  if (taskText !== "") {
+    const taskItem = document.createElement("li");
+    taskItem.innerHTML = `
+      ${taskText}
+      <button class="deleteBtn">Delete</button>
+    `;
+    taskList.appendChild(taskItem);
+    taskInput.value = "";
+    
+    const deleteBtn = taskItem.querySelector(".deleteBtn");
+    deleteBtn.addEventListener("click", () => {
+      taskList.removeChild(taskItem);
+    });
+  }
+});
+
+
+
